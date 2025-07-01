597d8a530a7d1bb4610d8c01880d58a1316ee45a
JaehongLIM2 <nu0044@naver.com>
2025-07-01 11:18:57 +0900

    props 의 타입은 뭐든지 {} 사용, String만 생략가능

---
+++ src/App7.jsx
@@ -0,0 +1,38 @@
+function MyComp1({ color, score, city, kids, skills, person }) {
+  console.log("color", color);
+  console.log("score", score);
+  console.log("city", city);
+  console.log("kids", kids);
+  console.log("skills", skills);
+  console.log("person", person);
+  return null;
+}
+
+// props 의 타입은 뭐든지 가능
+// String 은 ""
+// 나머지는 {}
+
+function MyComp2({ name, age, address, married }) {
+  console.log("name", name);
+  console.log("age", age);
+  console.log("address", address);
+  console.log("married", married);
+  return null;
+}
+
+function App7() {
+  return (
+    <>
+      <MyComp1
+        color="yellow"
+        score={123.123}
+        city="seoul"
+        kids={true}
+        skills={["javascript", "react", "typescript"]}
+        person={{ name: "son", age: 33, team: "토트넘" }}
+      />
+      <MyComp2 name="lee" age={33} address="jeju" married={true} />
+    </>
+  );
+}
+export default App7;
