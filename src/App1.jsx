560985800b8411a8d4461984226cfb7852b7253f
JaehongLIM2 <nu0044@naver.com>
2025-07-01 09:37:53 +0900

    리액트 코드 연습

---
+++ src/App1.jsx
@@ -0,0 +1,9 @@
+function App1() {
+  return (
+    <>
+      <p>두 번째 만드는 리액트 코드</p>
+    </>
+  );
+}
+export default App1;
+2;
---
+++ src/App2.jsx
@@ -0,0 +1,8 @@
+function App2() {
+  return (
+    <>
+      <h1>내가 만든 리액트 코드</h1>
+    </>
+  );
+}
+export default App2;
