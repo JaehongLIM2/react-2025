98886abb8407660c9c969c77e27a230ae24a0e61
JaehongLIM2 <nu0044@naver.com>
2025-07-07 16:33:10 +0900

    put 요청, delete 요청

--- src/App55.jsx
+++ src/App55.jsx
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import axios from "axios";
 
 function App55(props) {
+  const [myFile, setMyFile] = useState("");
+
   function handleButton1Click() {
     axios.put("/api/main30/sub1");
   }
@@ -32,8 +34,38 @@ function App55(props) {
     });
   }
 
+  function handleButton6Click() {
+    axios.delete("/api/main30/sub6", {
+      params: {
+        name: "John Doe",
+        location: "New York",
+        country: "United States",
+      },
+    });
+  }
+
+  function handleButton7Click() {
+    // file 전송시 postForm, 또는 putForm
+    axios.postForm("api/main30/sub7", {
+      name: "흥민 손",
+      age: 77,
+      myFile: myFile,
+    });
+  }
+
   return (
     <div>
+      <div>
+        <input
+          type="file"
+          value={myFile}
+          onChange={(e) => setMyFile(e.target.value)}
+        />
+        <hr />
+        <button onClick={handleButton7Click}>7 POST w/ file</button>
+      </div>
+      <button onClick={handleButton6Click}>6 DELETE 요청</button>
+      <hr />
       <button onClick={handleButton5Click}>5 PUT 요청</button>
       <hr />
       <button onClick={handleButton4Click}>4 DELETE 요청</button>
